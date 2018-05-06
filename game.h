784c6077df3c8853bfef2870f7bad08c459c4811
Гриша Тарасенко <fle4er@yandex.ru>
2018-05-06 21:17:36 +0700

    Начало: создан заголовочник, прописываю делёжку изображения на части

---
+++ game.h
@@ -0,0 +1,22 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <string>
+#include <vector>
+#include <SDL2/SDL.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+class Game
+{
+    private:
+        std::vector<SDL_Texture *> tiles;
+
+        void setTiles(std::string image, int rows, int cols);
+
+    public:
+        Game();
+};
+
+#endif
